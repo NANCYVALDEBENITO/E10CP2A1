0e04833c43fb222d96387a3a217aa1b9a56a4b89
nvaldebenito <nvaldebenito@localhost.localdomain>
2019-01-09 20:02:21 -0300

    add Ejercicio2

--- Ejercicio2.rb
+++ Ejercicio2.rb
@@ -1,11 +1,40 @@
+module Formula
+  def perimetro(l1, l2)
+    2 * (l1 + l2)
+  end
+
+  def area(l1, l2)
+    l1 * l2
+  end
+end
+
 class Rectangulo
+  include Formula
   def initialize(largo, ancho)
     @largo = largo
     @ancho = ancho
   end
+
+  def lados
+    puts @largo
+    puts @ancho
+  end
 end
+
 class Cuadrado
+  include Formula
   def initialize(lado)
     @lado = lado
   end
+
+  def lados
+    puts @lado
+  end
 end
+
+x = Cuadrado.new(3)
+y = Rectangulo.new(4, 3)
+puts x.perimetro(3, 3)
+puts x.area(3, 3)
+puts y.perimetro(4, 3)
+puts y.area(4, 3)
